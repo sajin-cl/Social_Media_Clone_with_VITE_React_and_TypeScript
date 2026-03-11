f416f7c134efa547196b2cd073b72683bb549032
Sajincl-03 <sajincl222@gmail.com>
2026-03-11 23:15:08 +0530

    responsive navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,19 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "./components/Navabar";
-import Suggestions from "./components/Suggestions";
+import Navbar from "@/components/Navbar";
+import Suggestions from "@/components/Suggestions";
 
 function App() {
 
 
   return (
-    <section className="flex min-h-screen">
+    <section className="flex min-h-screen bg-darkbg">
+      <nav className="w-20 hover:w-48 min-h-screen border-r bg-darkbg p-5 group transition-all duration-400">
+
       <Navbar />
+      </nav>
 
       <div className="flex flex-1 justify-center min-w-0">
-        <div className="w-full lg:max-w-2xl ">
+        <div className="w-full lg:max-w-4xl">
           <Outlet />
         </div>
       </div>
